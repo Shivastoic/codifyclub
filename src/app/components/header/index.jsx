9b61b28dd4c439814e0c7db828d1b9414d16263d
Shiva pandey <shivapandey9898@gmail.com>
2024-09-27 20:15:23 +0530

    feat: navbar complete, few changes remain

---
+++ src/app/components/header/index.jsx
@@ -0,0 +1,45 @@
+'use client'
+import styles from './style.module.scss'
+import { useEffect, useState } from 'react';
+import Nav from './nav';
+import { AnimatePresence } from 'framer-motion';
+import { usePathname } from 'next/navigation';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Header() {
+
+    const [isActive, setIsActive] = useState(false);
+    const pathname = usePathname();
+
+    useEffect( () => {
+
+      	if(isActive) setIsActive(false)
+
+    }, [pathname])
+
+    return (
+
+		<header>
+			<div className="fixed top-0 right-0 flex items-center justify-between w-full z-50 p-4">
+				<Link href="/">
+					<Image
+						className="lg:w-32 w-28 z-30"
+						src="/logos/codify-black.png"
+						width={100}
+						height={50}
+						alt="logo"
+					/>
+				</Link>
+				<div onClick={() => {setIsActive(!isActive)}} className={`size-14 flex items-center justify-center cursor-pointer rounded-full ${isActive ? "bg-white" : "bg-blue-400"}`}>
+					<div className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}></div>
+				</div>
+			</div>
+			<AnimatePresence mode="wait">
+				{isActive && <Nav />}
+			</AnimatePresence>
+		</header>
+
+    )
+
+}
