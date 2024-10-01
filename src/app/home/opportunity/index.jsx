ed5f9c4f3466e46b744ce8a46961e7e31fa0064e
Shiva pandey <shivapandey9898@gmail.com>
2024-10-01 18:49:36 +0530

    about section

--- src/app/home/opportunity/index.jsx
+++ src/app/home/opportunity/index.jsx
@@ -1,6 +1,8 @@
 import Container from "@/app/components/container";
 import Image from "next/image";
 import { Gradient } from "@/app/components/gradient";
+import PrimaryButton from "@/app/components/buttons/primary";
+import Link from "next/link";
 
 const data = {
 
@@ -14,11 +16,11 @@ export default function Opportunity(){
 
     return (
 
-        <section className="relative sm:py-16 lg:py-24 bg-dark overflow-hidden">
+        <section className="relative py-8 sm:py-16 lg:py-24 bg-dark overflow-hidden">
             <Gradient />
-            <div class="hidden absolute top-0 left-7.5 right-7.5 h-[1px] bg-neutral-600/25 undefined pointer-events-none lg:block xl:left-10 right-10"></div>
-            <div class="hidden absolute top-0 left-5 w-[1px] h-full bg-neutral-600/25 pointer-events-none md:block lg:left-7.5 xl:left-10"></div>
-            <div class="hidden absolute top-0 right-5 w-[1px] h-full bg-neutral-600/25 pointer-events-none md:block lg:right-7.5 xl:right-10"></div>
+            <div className="hidden absolute top-0 left-7.5 right-7.5 h-[1px] bg-neutral-600/25 undefined pointer-events-none lg:block xl:left-10 right-10"></div>
+            <div className="hidden absolute top-0 left-5 w-[1px] h-full bg-neutral-600/25 pointer-events-none md:block lg:left-7.5 xl:left-10"></div>
+            <div className="hidden absolute top-0 right-5 w-[1px] h-full bg-neutral-600/25 pointer-events-none md:block lg:right-7.5 xl:right-10"></div>
             <Container>
                 <div className="relative flex flex-col items-center justify-center gap-10">
                     <div className="flex max-w-[900px] py-10 z-10">
@@ -32,14 +34,19 @@ export default function Opportunity(){
                         alt="ball"
                         width={200}
                         height={200}
-                        className="size-52 absolute top-24 left-0 animate-bounceUpDown"  
+                        className="size-28 sm:size-40 lg:size-52 absolute top-52 md:top-24 left-0 animate-bounceUpDown"  
                     />
-                    <div className="grid grid-cols-3 gap-6 h-dvh w-full">
-                        <div className="w-full h-full rounded-xl bg-neutral-400/25 col-span-2 row-span-2"></div>
-                        <div className="w-full h-full rounded-xl bg-neutral-400/25"></div>
-                        <div className="w-full h-full rounded-xl bg-neutral-400/25"></div>
-                        <div className="w-full h-full rounded-xl bg-neutral-400/25"></div>
-                        <div className="w-full h-full rounded-xl bg-neutral-400/25 col-span-2"></div>
+                    <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 h-dvh w-full">
+                        <div className="w-full h-full rounded-xl bg-white/5 lg:col-span-2 row-span-2"></div>
+                        <div className="w-full h-full rounded-xl bg-white/5"></div>
+                        <div className="w-full h-full rounded-xl bg-white/5"></div>
+                        <div className="w-full h-full rounded-xl bg-white/5"></div>
+                        <div className="w-full h-full rounded-xl bg-white/5 lg:col-span-2"></div>
+                    </div>
+                    <div className="flex items-center justify-center md:pt-8">
+                        <Link href="">
+                            <PrimaryButton>Get started</PrimaryButton>
+                        </Link>
                     </div>
                 </div>
             </Container>
