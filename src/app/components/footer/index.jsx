c5dc34302e4a57f4d2c5bf7a2eccaaa3a720130b
Shiva pandey <shivapandey9898@gmail.com>
2024-10-13 16:04:57 +0530

    our app section

--- src/app/components/footer/index.jsx
+++ src/app/components/footer/index.jsx
@@ -4,12 +4,22 @@ export default function Footer(){
 
     return (
 
-        <footer className="bg-neutral-950 py-16">
-            <Container>
-                <div className="flex items-center justify-center">
-                    <h2 className="text-6xl text-white">Footer</h2>
+        <footer className="relative bg-light h-[500px]" style={{clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)"}}>
+            <div className="relative h-[calc(100vh+500px)] -top-[100vh]">
+                <div className="h-[500px] sticky top-[calc(100vh-500px)]">
+                    <Container>
+                        <div className="relative w-full h-full py-12 grid grid-cols-2 gap-6">
+                            <div>
+
+                            </div>
+                            <div className="flex items-end">    
+                                
+                            </div>
+                        </div>
+                    </Container>
+                    <h2 className="absolute bottom-0 left-0 text-5xl lg:text-[180px] font-syne font-bold tracking-tighter uppercase">Codify.</h2>
                 </div>
-            </Container>
+            </div>
         </footer>
 
     )
